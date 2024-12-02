993f564274974533441a4adc0fc0554031e252f0
nuggets ätaren <raskeb-1@student.ltu.se>
2024-12-02 10:38:24 +0100

    Fixed query statements in app

--- prototype/app.py
+++ prototype/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template
-from rdflib import Graph, Namespace
+from rdflib import Graph, Literal, Namespace
 from rdflib.plugins.sparql import prepareQuery
 from pathlib import Path
+import pprint
 
 app = Flask(__name__)
 
@@ -11,16 +12,25 @@ STIX = Namespace("http://stix.mitre.org/")
 EX = Namespace("http://example.org/")
 
 # Load RDF data
-
-
+results = []
 pathlist = Path("../rdf").glob('**/*.ttl')
 for path in pathlist:
     # because path is object not string
-    path_in_str = str(path)   
-    print(path_in_str)
+    path_in_str = str(path)
     g.parse(path_in_str, format="turtle")
-    
-    # print(path_in_str)
+    query = '''
+        SELECT ?name ?id ?manu ?teethGrade ?teethAmount WHERE {
+            ?instance rdf:type cmp:sawblade .
+            ?instance product:name ?name .
+            ?instance product:id ?id .
+            ?instance product:manufacturer ?manu .
+            ?instance cmp:sawblade:teethGrade ?teethGrade .
+            ?instance cmp:sawblade:teethAmount ?teethAmount .
+        }
+    '''
+    result = g.query(query)
+    results.append(result)
+
 
 @app.route('/')
 def index():
@@ -28,15 +38,29 @@ def index():
 
 @app.route('/search', methods=['POST'])
 def search():
-    query = request.form['query']
-    results = perform_sparql_query(query)
-    return render_template('search_results.html', results=results)
+    for row in result:
+        if Form == row:
+            return render_template('rdf_results.html', results=result)
 
 @app.route('/rdf', methods=['POST'])
 def rdf_query():
-    query = request.form['rdf_query']
-    results = perform_sparql_query(query)
-    return render_template('rdf_results.html', results=results)
+    Form = request.form['rdf_query']
+    desc = []
+    for instance in results:
+        for inst in instance:
+            print(inst)
+            name = str(inst['name'])
+            if Form.__eq__(name):
+                id = str(inst['id'])
+                manu = str(inst['manu'])
+                teethgrade = str(inst['teethGrade'])
+                teethamount = str(inst['teethAmount'])
+                desc.append(name)
+                desc.append(id)
+                desc.append(manu)
+                desc.append(teethgrade)
+                desc.append(teethamount)
+        return render_template('rdf_results.html', results=desc)
 
 def perform_sparql_query(query):
     q = prepareQuery(query)
