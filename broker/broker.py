fc982f4fd7ed473bd27a076aae922d1a1cff625b
Anton Nyström <35500074+antonnyst@users.noreply.github.com>
2024-12-03 17:39:06 +0100

    Add basic broker

---
+++ broker/broker.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask import request
+import requests
+
+DB_URI = "http://localhost:7200"
+REPO_NAME = "ceb"
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Endpoints: /register /unregister /abandon /query"
+
+@app.post("/unregister")
+def unregister():
+    # TODO
+    return "OK"
+
+@app.post("/register")
+def register():
+    # Maybe file upload
+    # TODO
+    return "OK"
+
+@app.post("/abandon") 
+def abandon():
+    # TODO
+    return "OK"
+
+@app.post("/query")
+def query():
+    # Get query
+    sparql_query = request.form['query']
+    
+    # Check authentication?
+    # TODO
+
+    # Send to DB
+    headers = {'Accept': 'application/json', "content-type": "application/x-www-form-urlencoded"}
+    
+    response = requests.post(DB_URI+"/repositories/"+REPO_NAME, data={'query': sparql_query}, headers=headers)
+    
+    # Return
+    return response.content
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=7100)
