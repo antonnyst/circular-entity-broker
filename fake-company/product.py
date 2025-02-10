eb68179542de0750a9a50066436f89ac4ee0637f
add33m <48681209+add33m@users.noreply.github.com>
2025-02-10 22:59:22 +0100

    Started implementing company invokes and reg

--- fake-company/product.py
+++ fake-company/product.py
@@ -18,3 +18,17 @@ class Product:
     
   def toJson(self):
     return json.dumps(self, default=lambda o: o.__dict__)
+  
+  # Convert product data into an array of properties that can be sent to the broker
+  def toProperties(self):
+    properties = []
+
+    for propName, value in self.__dict__.items():
+      value_is_number = type(value) == float or type(value) == int
+      properties.append({
+        "valueType": value_is_number and "float" or "string",
+        "property": propName,
+        "value": str(value),
+      })
+
+    return properties
