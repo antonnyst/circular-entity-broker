f4c596d98cf37b21111356b963c53acd7e8ed473
add33m <48681209+add33m@users.noreply.github.com>
2025-02-11 11:11:38 +0100

    Invoke product creation works

--- fake-company/product.py
+++ fake-company/product.py
@@ -7,14 +7,16 @@ class Product:
   price = 0
   stock = 0
   creation_date = "01/01/70"
+  manufacturer = ""
   
-  def __init__(self, pid, name, description, price, stock, creation_date):
+  def __init__(self, pid, name, description, price, stock, creation_date, manufacturer):
     self.product_id = pid
     self.name = name
     self.description = description
     self.price = price
     self.stock = stock
     self.creation_date = creation_date
+    self.manufacturer = manufacturer
     
   def toJson(self):
     return json.dumps(self, default=lambda o: o.__dict__)
@@ -23,12 +25,28 @@ class Product:
   def toProperties(self):
     properties = []
 
-    for propName, value in self.__dict__.items():
-      value_is_number = type(value) == float or type(value) == int
-      properties.append({
-        "valueType": value_is_number and "float" or "string",
-        "property": propName,
-        "value": str(value),
-      })
+    # for propName, value in self.__dict__.items():
+    #   value_is_number = type(value) == float or type(value) == int
+    #   properties.append({
+    #     "valueType": value_is_number and "float" or "string",
+    #     "property": propName,
+    #     "value": str(value),
+    #   })
+
+    properties.append({
+      "valueType": "string",
+      "property": "id",
+      "value": self.product_id
+    })
+    properties.append({
+      "valueType": "string",
+      "property": "name",
+      "value": self.name
+    })
+    properties.append({
+      "valueType": "string",
+      "property": "manufacturer",
+      "value": self.manufacturer
+    })
 
     return properties
