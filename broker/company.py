75e6913f60f8be4a8d10777e1caca9a85686d47a
Anton Nyström <35500074+antonnyst@users.noreply.github.com>
2025-02-25 17:07:14 +0100

    Split company endpoints into seperate file

---
+++ broker/company.py
@@ -0,0 +1,69 @@
+from app import app
+from flask import request
+import uuid
+import os
+
+import db
+# For all the company endpoint lovers! 🥰💅
+
+
+def generate_companyId():
+    return uuid.uuid4().hex
+
+def generate_accessToken():
+    return os.urandom(64).hex()
+
+
+# Register a new company and send back access token
+@app.post("/register")
+def company_register():
+    company_name = request.json["name"]
+    
+    companyId = generate_companyId();
+
+    accessToken = generate_accessToken();
+
+    company_properties = [
+        ["name", company_name, "string"],
+        ["accessToken", accessToken, "string"]
+    ] 
+
+    db.add_company(companyId, company_properties);
+
+    return {
+        "companyId": companyId,
+        "accessToken": accessToken
+    }
+
+@app.get("/interrogation")
+def company_interrogation_get():
+    query = """ 
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        select * where {
+            ?s rdfs:subClassOf <http://ceb.ltu.se/broker/product> .
+            FILTER (?s != <http://ceb.ltu.se/broker/product>)
+        }
+    """
+
+
+    pass
+
+@app.post("/interrogation")
+def company_interrogation_post():
+    url = request.json["url"]
+    
+
+
+
+    # TODO add access token verification
+
+
+    db.add_company_url()
+
+
+    pass
+
+@app.delete("/interrogation")
+def company_interrogation_delete():
+    pass
